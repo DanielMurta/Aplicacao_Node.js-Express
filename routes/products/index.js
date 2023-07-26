8ccc7f661b19d29ed7827f2149a3a2705dbe6af8
Daniel Silveira <murtdaniel@gmail.com>
2023-07-26 16:23:39 -0300

    rota product/delete/:id

--- routes/products/index.js
+++ routes/products/index.js
@@ -12,13 +12,24 @@ router.post('/save', middlewareAuth, authRole, (req, res) => {
     conn.query(`INSERT INTO products (name, description, price, user_id) VALUES ('${name}', '${description}', ${price}, ${userId});`, 
     (err) => {
         if (err) {
-            console.log(err)
             return res.status(500).send({ error: 'Internal server error'})
         }
 
     })
 
     return res.sendStatus(201)
+})
+
+router.delete('/delete/:id', middlewareAuth, authRole, (req, res) => {
+    const productUser = req.params.id
+
+    conn.query(`DELETE FROM products WHERE id = ${productUser};`, (err) => {
+        if (err) {
+            return res.status(500).send({ error: 'Internal server error' })
+        }
+    })
+
+    return res.sendStatus(204)
 })
 
 module.exports = router
