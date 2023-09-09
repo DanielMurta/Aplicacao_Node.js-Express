36f8e9ef9822371e66397edb79574e4352707c09
Daniel Silveira <murtdaniel@gmail.com>
2023-09-09 16:44:53 -0300

    Configurando variaveis de ambiente

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: 'daniel12345678910',
-    database: 'marketplace'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
 module.exports = pool
